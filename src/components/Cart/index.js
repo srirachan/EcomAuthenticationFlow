5a2f1c4c7ade4f468016744b29570760d99fa4a0
Sri rachan <srirachan0357@gmail.com>
2022-06-22 13:30:28 +0000

    first commit

---
+++ src/components/Cart/index.js
@@ -0,0 +1,14 @@
+// Write your JS code here
+import './index.css'
+
+const Cart = () => (
+  <div>
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-img.png"
+      alt="cart"
+      className="cart-image"
+    />
+  </div>
+)
+
+export default Cart
---
+++ src/components/Products/index.js
@@ -0,0 +1,15 @@
+// Write your JS code here
+
+import './index.css'
+
+const Products = () => (
+  <div>
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
+      alt="products"
+      className="product-image"
+    />
+  </div>
+)
+
+export default Products
